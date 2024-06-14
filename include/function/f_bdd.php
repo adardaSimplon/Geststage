38aa2fe35e0bde6f1a7804fd242a51b3e87e0318
adardaSimplon <adarda.ext@simplonformations.onmicrosoft.com>
2024-06-14 11:23:33 +0200

    Add SSL

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -2,16 +2,28 @@
 
 function connexionBDD()
 {
-	try
-	{
-            $bdd = new PDO('mysql:host=bdd-anna.mysql.database.azure.com;port=3306;dbname=bdd_geststages;charset=utf8', 'usergs', 'mdpGS', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    try
+    {
+        $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem' // Chemin vers le certificat RootCA de Azure
+            );
+    
+            $bdd = new PDO(
+            'mysql:host=bdd-anna.mysql.database.azure.com;port=3306;dbname=bdd_geststages;charset=utf8',
+            'usergs',
+            'mdpGS',
+            $options
+        );
+            //array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        
             return $bdd;
-	}
-	catch(Exception $e)
-	{
-		$pdo_error = $e->getMessage();
+    }
+    catch(Exception $e)
+    {
+        $pdo_error = $e->getMessage();
                 return false;
-	}
+    }
     
 }
 
